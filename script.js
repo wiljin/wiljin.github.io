9ba74a424ade1fc6c934305ec2df83ed4fc3273e
wiljin <willymjin@gmail.com>
2015-01-19 23:56:11 -0500

    customized more

--- script.js
+++ script.js
@@ -4,6 +4,15 @@ $('.dropdown-toggle').click(function(){
     $('.dropdown-menu').toggle();
 });
 
+$('.arrow-next').click(function(){
+    var current_dot = $('.active-dot');
+    var next_dot = current.next();
+    if(next_dot == 0)
+        next_dot = $('.dot').first();
+    current_dot.removeClass('active');
+    next_dot.addClass('active');
+    
+});
 };
 
 $(document).ready(main);
