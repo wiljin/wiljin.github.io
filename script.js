562a8f02971be042ff9dc91aa9da56028deaf9d9
wiljin <willymjin@gmail.com>
2015-01-20 00:15:23 -0500

    fixed forward typo

--- script.js
+++ script.js
@@ -8,14 +8,14 @@ $('.arrow-next').click(function(){
     var current_slide = $('.active-slide');
     var next_slide = current_slide.next();
     var current_dot = $('.active-dot');
-    var next_dot = current.next();
+    var next_dot = current_dot.next();
     if(next_slide.length === 0)
     {
         next_slide = $('.slide').first();
         next_dot = $('.dot').first();
     }   
-    currentSlide.fadeOut(500).removeClass('active-slide');
-    nextSlide.fadeIn(500).addClass('active-slide');
+    current_slide.fadeOut(500).removeClass('active-slide');
+    next_slide.fadeIn(500).addClass('active-slide');
     current_dot.removeClass('active-dot');
     next_dot.addClass('active-dot');
     
